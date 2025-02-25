3c04c1eea632bf68423aa8a52f3646244bea0be6
habib-676 <rahman.habibur2421k@gmail.com>
2025-02-25 19:59:42 +0600

    login to main is done successfully

--- scripts/login.js
+++ scripts/login.js
@@ -2,4 +2,15 @@ document
   .getElementById("login-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
+    const accNum = document.getElementById("account-num").value;
+    const accPin = document.getElementById("account-pin").value;
+    if (accNum.length === 11) {
+      if (accPin.length === 4) {
+        window.location.href = "./main.html";
+      } else {
+        alert("Enter 4 digit pin");
+      }
+    } else {
+      alert("Enter a valid id");
+    }
   });
