5d19e630b9d2cb83b006ff52cb663665fef5801a
habib-676 <rahman.habibur2421k@gmail.com>
2025-02-27 02:01:28 +0600

    add money's script added

--- scripts/addMoney.js
+++ scripts/addMoney.js
@@ -2,4 +2,13 @@ document
   .getElementById("add-money-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
+    const amount = parseFloat(document.getElementById("amount").value);
+    const pin = parseInt(document.getElementById("pin").value);
+    let balance = parseFloat(document.getElementById("main-balance").innerText);
+
+    if (pin === 1234) {
+      document.getElementById("main-balance").innerText = amount + balance;
+    } else {
+      alert("This pin is not correct !!!!!!");
+    }
   });
